9b912fa648ffa8fae5e66241e7fc1ce3491705a2
PrabakaranElango <85854671+PrabakaranElango@users.noreply.github.com>
2021-06-14 13:17:25 +0530

    Lennox_assignment

---
+++ SeleniumAssessment/src/main/java/com/seleniumassessment/SeleniumAssessment/TestCase1.java
@@ -0,0 +1,18 @@
+package com.seleniumassessment.SeleniumAssessment;
+
+public class TestCase1 {
+	
+	static BaseMethods basemethod = new BaseMethods();
+
+	public static void main(String args[]) {
+	
+	basemethod.startChrome();
+	basemethod.geturl();
+	basemethod.login();
+	basemethod.navigation();
+	basemethod.checksubcatagory("10T46");
+	basemethod.checkproductname("Bristol H22J38BABC, Reciprocating Compressor, 37,800 Btuh 208/230V, R-22, 1 Phase");
+	basemethod.checkproductprice("Your Price: $1173.00 |List price: $2697.");
+	basemethod.driverclose();
+	}
+}
